dba3ebb69e2722ef5cd47f7446207692255c1871
nagasahikuthuru231-cyber <nagasahikuthuru231@gmail.com>
2026-08-09 16:52:12 +0530

    designed and developed frontend input with vibrant ui styling.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,41 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+ import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [url, setUrl] = useState("")
+function onSubmit(){
+   window.alert(url)
+   setUrl("")
+}
   return (
-    
-    <>
-    <div>Hi there!</div>
-    </>
+    <div className="app">
+
+      <div className="shortener-card">
+
+        <h1>Link Shortener</h1>
+
+        <p>
+          Turn your long URLs into short, shareable links.
+        </p>
+
+        <div className="input-container">
+
+          <input
+            type="text"
+            placeholder="Enter your URL..."
+            value={url}
+            onChange={(e) => setUrl(e.target.value)}
+          />
+
+          <button  onClick={onSubmit}>
+            Shorten
+          </button>
+
+        </div>
+
+      </div>
+
+    </div>
       
   )
 }
